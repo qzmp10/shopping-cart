2943ec7a42897726967febed029162c9197d661e
qzmp10 <loganrobitaille@outlook.com>
2022-10-27 12:13:05 -0400

    new tests, new component

--- src/App.test.js
+++ src/App.test.js
@@ -31,3 +31,20 @@ describe('home container', () => {
     expect([header, underHeader, shop].length).toBe(3);
   })
 })
+
+describe('product container', () => {
+  it('product page has header', () => {
+    render(<Products />);
+
+    const header = screen.getByRole('heading');
+
+    expect(header.textContent).toMatch('Products')
+  })
+
+  it('product page has 4 product divs', () => {
+    render(<Products />);
+    const productDivArray = screen.getAllByTestId('productDiv');
+
+    expect(productDivArray.length).toBe(4);
+  })
+})
---
+++ src/components/Vegetable.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect, useRef } from 'react'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+
+export default function Vegetable(props) {
+    return (
+        <div className="vegetable"></div>
+    )
+}
