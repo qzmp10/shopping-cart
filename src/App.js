a6447caa499fbbc6b43b90d7ff766a3141075878
qzmp10 <loganrobitaille@outlook.com>
2022-10-27 12:41:29 -0400

    css, link to product page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Nav from './components/Nav';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Products from './components/Products';
 import Home from './components/Home';
+import ProductPage from './components/ProductPage';
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />}></Route>
           <Route path='/products' element={<Products />}></Route>
+          <Route path='/products/:veggie' element={<ProductPage />}></Route>
         </Routes>
       </div>
     </BrowserRouter>
--- src/components/Vegetable.js
+++ src/components/Vegetable.js
@@ -3,10 +3,13 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 
 export default function Vegetable(props) {
     return (
-        <div className="vegetable" data-testid='productDiv'>
-            <img className='img'/>
-            <div className ='text'>Text</div>
-            <div className='price'>Price</div>
-        </div>
+        <Link to='/products/veggie/'>
+            <div className="vegetable" data-testid='productDiv'>
+                <img className='img' alt='veggie'/>
+                <div className='text'>Text</div>
+                <div className='price'>Price</div>
+            </div>
+        </Link>
+
     )
 }
